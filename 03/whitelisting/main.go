1ee785cd293675e03f346eb7fee541e5f030b71e
Paul Mooney <paul@gmst.ie>
2023-03-13 22:34:07 +0000

    Converted console app to web server

--- 03/whitelisting/main.go
+++ 03/whitelisting/main.go
@@ -2,25 +2,33 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	// Define a list of allowed values
+	// Define a map of allowed values
 	allowedValues := map[string]bool{
 		"foo": true,
 		"bar": true,
 		"baz": true,
 	}
 
-	// Get user input
-	var userInput string
-	fmt.Println("Enter a value:")
-	fmt.Scanln(&userInput)
+	// Define the HTTP handler function
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Get user input from query parameter
+		userInput := r.URL.Query().Get("input")
 
-	// Check if user input is in the list of allowed values
-	if _, ok := allowedValues[userInput]; ok {
-		fmt.Println("Valid input.")
-	} else {
-		fmt.Println("Invalid input.")
+		// Check if user input is in the map of allowed values
+		if _, ok := allowedValues[userInput]; ok {
+			fmt.Fprintf(w, "Valid input: %s", userInput)
+		} else {
+			fmt.Fprint(w, "Invalid input")
+		}
+	})
+
+	// Start the web server
+	fmt.Println("Listening on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
 	}
 }
