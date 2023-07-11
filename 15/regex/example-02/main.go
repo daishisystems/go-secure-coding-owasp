6260fb95f109929725757be10faa067baf954b6f
Paul Mooney <paul@gmst.ie>
2023-07-11 22:08:05 +0100

    Changed initial regex example to simulate error 01

--- 15/regex/example-02/main.go
+++ 15/regex/example-02/main.go
@@ -9,7 +9,9 @@ func main() {
 	testString1 := "<h1>Go Secure Coding Practices Guide</h1>"
 	testString2 := "<p>Go Secure Coding Practices Guide</p>"
 	testString3 := "<h1>Go Secure Coding Practices Guid</p>"
-	regex := regexp.MustCompile("<([a-z][a-z0-9]*)\b[^>]*>.*?<\\/\\1>")
+	// todo: Change to the following to 'fix' unknown escape sequence error:
+	// <([a-z][a-z0-9]*)\b[^>]*>.*?<\\/\\1>
+	regex := regexp.MustCompile("<([a-z][a-z0-9]*)\b[^>]*>.*?<\/\1>")
 
 	fmt.Println(regex.MatchString(testString1))
 	fmt.Println(regex.MatchString(testString2))
